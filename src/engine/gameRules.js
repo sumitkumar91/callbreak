c45ae6356827e08f6eeed51873023cbdd5478f6b
Sumit Kumar <74456338+sumitkumar91@users.noreply.github.com>
2026-05-28 21:19:31 +0530

    Rule fix

--- src/engine/gameRules.js
+++ src/engine/gameRules.js
@@ -1,37 +1,5 @@
 // Call Break game rules and validations
 
-// Determines if a card play is valid based on the current trick and hand
-export const isValidPlay = (card, currentTrickCards, playerHand) => {
-  // If first to play, any card is valid
-  if (currentTrickCards.length === 0) return true;
-
-  const leadSuit = currentTrickCards[0].card.suit;
-  
-  // Does the player have the lead suit?
-  const hasLeadSuit = playerHand.some(c => c.suit === leadSuit);
-
-  if (hasLeadSuit) {
-    // Must follow suit
-    return card.suit === leadSuit;
-  }
-
-  // If player doesn't have the lead suit, check if they have a Spade
-  const hasSpade = playerHand.some(c => c.suit === 'Spades');
-
-  if (hasSpade) {
-    // Must play a spade if they have one
-    return card.suit === 'Spades';
-  }
-
-  // If they have neither the lead suit nor spades, they can play any card
-  return true;
-};
-
-// Gets valid cards a player can play
-export const getValidCards = (currentTrickCards, playerHand) => {
-  return playerHand.filter(card => isValidPlay(card, currentTrickCards, playerHand));
-};
-
 // Determines the winning card in a trick
 export const determineTrickWinner = (trickCards) => {
   if (trickCards.length === 0) return null;
@@ -61,6 +29,60 @@ export const determineTrickWinner = (trickCards) => {
   }
 
   return winningCard;
+};
+
+// Gets strictly valid cards a player can play based on obligation to win
+export const getValidCards = (currentTrickCards, playerHand) => {
+  // If first to play, any card is valid
+  if (currentTrickCards.length === 0) return playerHand;
+
+  const leadSuit = currentTrickCards[0].card.suit;
+  const currentWinner = determineTrickWinner(currentTrickCards);
+
+  // Does the player have the lead suit?
+  const cardsOfLeadSuit = playerHand.filter(c => c.suit === leadSuit);
+
+  if (cardsOfLeadSuit.length > 0) {
+    // Must follow suit
+    // Must try to win if possible
+    if (currentWinner.card.suit === leadSuit) {
+      const winningValue = currentWinner.card.value;
+      const higherCards = cardsOfLeadSuit.filter(c => c.value > winningValue);
+      if (higherCards.length > 0) {
+        return higherCards; // Obligation to beat the current highest card
+      }
+    }
+    // Either a spade is winning, or we can't beat the highest lead suit card
+    return cardsOfLeadSuit;
+  }
+
+  // If player doesn't have the lead suit, check if they have a Spade
+  const spades = playerHand.filter(c => c.suit === 'Spades');
+
+  if (spades.length > 0) {
+    // Must play a spade
+    let winningSpadeValue = 0;
+    if (currentWinner.card.suit === 'Spades') {
+      winningSpadeValue = currentWinner.card.value;
+    }
+    
+    const higherSpades = spades.filter(c => c.value > winningSpadeValue);
+    if (higherSpades.length > 0) {
+      return higherSpades; // Obligation to beat the current winning spade
+    }
+    
+    // We can't beat the winning spade, but must still play a spade
+    return spades;
+  }
+
+  // If they have neither the lead suit nor spades, they can play any card
+  return playerHand;
+};
+
+// Determines if a card play is valid based on the current trick and hand
+export const isValidPlay = (card, currentTrickCards, playerHand) => {
+  const validCards = getValidCards(currentTrickCards, playerHand);
+  return validCards.some(c => c.id === card.id);
 };
 
 // Calculate scores at the end of a round
